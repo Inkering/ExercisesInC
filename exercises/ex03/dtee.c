ea7485f7aea87a6c89615061ba0b93d7dbd12396
DIETER BREHM <inkering@gmail.com>
2020-02-16 12:45:40 -0500

    add scaffolding for my tee version

---
+++ exercises/ex03/dtee.c
@@ -0,0 +1,41 @@
+/*
+	 dtee, pronounced dee-tee
+	 A tee copy by Dieter Brehm, 2020
+
+	 Softsys Questions:
+	 3. REFLECT
+
+	 4. SOLUTION COMPARISON
+
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+/*
+	chunk of code for writing any found
+	standard input to a given file pointer.
+ */
+void writeContents(FILE* fileOutput) {
+	// * check file?
+	// * write all of found stdin until it's NULL
+	// * close the file?
+	// * print the output too
+}
+
+/*
+	check for errors and incorrect arguments
+
+ */
+void argCheck(int argc, char* argv) {
+	// * wrong number of arguments
+	// * not valid argument
+	// * bad characters in argument?
+}
+
+int main() {
+	// * look for errors
+	// * attempt to open file with either w or a mode if append only
+	// * look for stdin stuff (writeContents func)
+	// * close file?
+}
