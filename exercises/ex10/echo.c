f1f750fdc2048635058b6b37730fc18124b057d1
Dieter Brehm <inkering@gmail.com>
2020-04-24 13:58:47 -0400

    update hw10

--- exercises/ex10/echo.c
+++ exercises/ex10/echo.c
@@ -37,10 +37,10 @@ void parent_code(int pipe_to_child[], int pipe_to_parent[])
     char buffer[count];
 
     // read from pipe_to_child
-    ssize_t sizeread = read(pipe_to_parent[0], buffer, count);
-    if (size == -1)
-        error("Child can't read from parent");
-    printf("Parent read %ld bytes.\n", sizeread);
+    ssize_t rsz = read(pipe_to_parent[0], buffer, count);
+    if (rsz == -1)
+        error("parent can't read from child");
+    printf("Parent read %ld bytes.\n", rsz);
     printf("Parent read: %s\n", buffer);
 }
 
@@ -61,11 +61,11 @@ void child_code(int pipe_to_child[], int pipe_to_parent[])
     printf("Child read: %s\n", buffer);
 
 		//close read end of pipe to parent
-		close(pipe_to_parent[0]);
+		/* close(pipe_to_parent[0]); */
 
 		// write a string to the child
-    char line[] = "I Love you, parent";
-    ssize_t sizewrite = write(pipe_to_parent[1], line, strlen(line)+1);
+    char send[] = "I Love you, parent";
+    ssize_t sizewrite = write(pipe_to_parent[1], send, strlen(send)+1);
     if (sizewrite == -1)
         error("Child can't write to parent");
     printf("Child wrote %ld bytes\n", sizewrite);
@@ -78,10 +78,11 @@ int main(int argc, char *argv[])
 {
     /*Create a pipe */
     int pipe_to_child[2];
+		int pipe_to_parent[2];
+		
     if (pipe(pipe_to_child) == -1)
         error("Can't create the first pipe");
 
-		int pipe_to_parent[2];
 		if (pipe(pipe_to_parent) == -1)
 			error("Can't create the second pipe");
 
